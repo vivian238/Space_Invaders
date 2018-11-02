6baee6ab5f310f33a52f5b52e4851f8201d9c847
Vivian <42046607+vivian238@users.noreply.github.com>
2018-11-02 00:40:56 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,16 +5,36 @@
 --]]
 
 function love.load(arg)
+  -- set cool pixel font (downloaded from here: https://www.dafont.com/04b-03.font)
+  my_font = love.graphics.setNewFont("font.TTF", 20)
+  
+  love.window.setMode(640, 480, {resizable=true, vsync=false})  -- note: default window size is 800 x 600
 end
 
 function love.keyreleased(key)
-  
+  -- if player presses space bar, a bullet is shot upwards.
+  if (key == " " or key == "space") then
+    shoot()
+  end
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+  -- sets title of new window
+  love.window.setTitle("Space Invaders")
+  
   -- sets background color to black
   love.graphics.setBackgroundColor(0,0,0)
+  
+  -- add green background border...just for aesthetics
+  love.graphics.setColor(0,255,0)
+  love.graphics.setLineWidth(10)
+  love.graphics.rectangle("line", 0, 0, 640, 480)
+  
+  -- print 'SCORE' in white on top left of screen
+  love.graphics.setColor(255,255,255)
+  love.graphics.print("SPACE INVADERS", 18, 18)
+  love.graphics.print("SCORE", 450, 18)
 end
