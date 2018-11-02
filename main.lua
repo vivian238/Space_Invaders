98b9844b4e2501882263dddce5fc321a4e0f7488
Vivian <42046607+vivian238@users.noreply.github.com>
2018-11-01 22:03:51 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,20 @@
+--[[
+• Code Written By Vivian
+• Space Invaders, by Tomohiro Nishikado, is a classic arcade game that released in 1978. This is
+  my attempt to recreate it in Lua Using the Love2D Game Engine. (✿◠‿◠)
+--]]
+
+function love.load(arg)
+end
+
+function love.keyreleased(key)
+  
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  -- sets background color to black
+  love.graphics.setBackgroundColor(0,0,0)
+end
